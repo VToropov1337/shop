6552764d49c5efecc7c2783ed1a3e95f5072c9a6
VToropov1337 <vladimirtoropov87@gmail.com>
2017-06-09 16:21:26 +0300

    save

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,37 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+
   end
 end
