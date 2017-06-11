3b74a26c8f324a547c00e9ddf5853c4f0ae540c9
VToropov1337 <vladimirtoropov87@gmail.com>
2017-06-11 13:15:07 +0300

    complete crud

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :find_post, only: [:create, :update, :edit, :destroy]
+
   def index
     @items = Item.all
   end
@@ -14,21 +16,31 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
+    redirect_to @item
   end
 
   def edit
-
   end
 
   def update
+    if  @item.update(item_params)
+        redirect_to @item
+    else
+      render 'edit'
+    end
 
   end
 
   def destroy
-
+    @item.destroy
+      redirect root_path
   end
 
   private
+
+  def find_post
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :price)
